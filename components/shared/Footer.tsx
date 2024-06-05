4885afb1480af2df207300af72c87cbe2d25e434
Tharaka Senevirathne <tharaka326@gmail.com>
2024-06-05 17:35:00 +0530

    updates

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-5' >
+      <div className='flex-center wrapper flex-between flex flex-col text-center sm:flex-row'>
+        <Link href='/'>
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38} />
+        </Link>
+
+        <p>2024 Your Choice. All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
